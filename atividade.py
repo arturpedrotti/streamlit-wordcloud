13f032457ce9b6e3ed4215492ef26bcdf28d456d
elonbouvier <elonbouvier@elonbouviers-MacBook-Air.local>
2023-05-28 14:10:19 -0300

    Adicionei o script atividades.py

--- atividade.py
+++ atividade.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
 
+def main():
+    st.title('CSV Viewer')
+    data = pd.read_csv('dados.csv')
+    st.dataframe(data)
+
+if __name__ == '__main__':
+    main()
